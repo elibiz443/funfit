90b91555b6a26c18c1584356446faad0fca3e4cc
ELLY AMBET <elibiz443@gmail.com>
2025-10-30 08:20:11 +0300

    Updated config

--- config.php
+++ config.php
@@ -1,19 +1,24 @@
 <?php
   if ($_SERVER['SERVER_NAME'] == 'localhost') {
     define('ROOT_URL', 'http://localhost/funfit');
+    $host = 'localhost';
+    $dbname = 'funfit_db';
+    $username = 'root';
+    $password = '';
+    define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/bibleville-web');
   } else {
-    define('ROOT_URL', 'https://funtwo.fit');
+    define('ROOT_URL', 'https://f2.riverbarncafe.com/');
+    $host = 'localhost';
+    $dbname = 'riverba1_funfit';
+    $username = 'riverba1_funfit';
+    $password = 'mKVteKFkzjHxrUxMZEU3';
+    define('ROOT_PATH', dirname(__FILE__));
   }
-
-  $host = 'localhost';
-  $dbname = 'funfit_db';
-  $username = 'root';
-  $password = '';
 
   try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
-    die("Could not connect to the database $dbname :" . $e->getMessage());
+    die("Could not connect to the database $dbname: " . $e->getMessage());
   }
 ?>
